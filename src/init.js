8508880268ffb647e326633d49bbb27ef9dc5e90
Jamen Marzonie <jamenmarz@gmail.com>
2016-01-25 04:43:29 -0700

    Add pose.init

--- src/init.js
+++ src/init.js
@@ -1,10 +1,32 @@
-// import fs from 'fs';
+import fs from 'fs';
+import path from 'path';
+import { Extract } from 'tar';
 
 /** pose.init
  * @desc Initiate a template.
+ * @param {Object} opts - Options
  */
-function init() {
+function init(opts = {}) {
+  const name = opts.name;
+  const templateName = opts._[1];
+  const entry = path.join(opts._[2] || opts.entry, name);
+  const template = path.join(opts._templates, `${templateName}.tar`);
 
+  const extract = new Extract({ path: entry });
+  const write = fs.createReadStream(template)
+  .pipe(extract);
+
+  extract.on('end', () => {
+    console.log(`Initiated ${name}.`);
+  });
+
+  extract.on('error', () => {
+    console.log(`Failed to extract ${templateName}`);
+  });
+
+  write.on('error', () => {
+    console.log(`Failed to write ${template}`);
+  });
 }
 
 export default init;
