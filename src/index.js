0f20d3eaad8120dcd724769a3978cf4408f265ad
Jamen Marzonie <jamenmarz@gmail.com>
2016-01-25 19:49:32 -0700

    Add aliases for actions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import init from './init';
 import help from './help';
 import save from './save';
+import list from './list';
 import remove from './remove';
 
-export { init, save, remove, help };
+// aliases
+const { i, s, rm, ls } = { init, save, remove, list };
+
+export {
+  init, i,
+  save, s,
+  list, ls,
+  remove, rm,
+  help,
+};
